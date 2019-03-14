2e29fa76bfa6c95131159218d0c0e16850ec2978
The-bambi <krystian.figiel@smcebi.edu.pl>
2019-03-14 20:40:07 +0100

    Futher improvements

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pygame
+from grid import *
 
 pygame.init()
 
-screen = pygame.display.set_mode((400, 600))
+screen = pygame.display.set_mode((400, 581))
 clock = pygame.time.Clock()
+grid = Grid(10, 20, screen)
 
 done = False
 
@@ -15,8 +17,13 @@ while not done:
                 done = True
 
     
-
     screen.fill((0,0,0))
+
+    grid.grid[5][3].up.color = (255,0,0)
+    grid.grid[5][3].down.color = (255,0,0)
+    grid.grid[5][3].right.color = (255,0,0)
+    grid.grid[5][3].left.color = (255,0,0)
+    grid.show()
 
     pygame.display.flip()
     clock.tick(120)
