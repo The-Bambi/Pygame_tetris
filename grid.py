f0919d014f79109c0fe0867842a9708d0b552cf1
The-bambi <krystian.figiel@smcebi.edu.pl>
2019-03-14 20:39:46 +0100

    Changed square to cell for consistency's sake

--- grid.py
+++ grid.py
@@ -1,19 +1,21 @@
-from square import *
+from cell import *
 
 class Grid:
 
-    def __init__(self, x_amount, y_amount):
-        self.grid = [[Square(x, y, 10, 10) for x in range(x_amount)] for y in range(y_amount)]
+    def __init__(self, x_amount, y_amount, screen):
+        self.grid = [[Cell(x*29, y*29, 30, 30, screen) for x in range(x_amount)] for y in range(y_amount)]
         for y, line in enumerate(self.grid):
             for x, cell in enumerate(line):
                 if y > 0:
-                    cell.neighbours.append(self.grid[y-1][x])
+                    cell.up = self.grid[y-1][x]
                 if y < y_amount - 1:
-                    cell.neighbours.append(self.grid[y+1][x])
+                    cell.down = self.grid[y+1][x]
                 if x > 0:
-                    cell.neighbours.append(self.grid[y][x-1])
+                    cell.left = self.grid[y][x-1]
                 if x < x_amount - 1:
-                    cell.neighbours.append(self.grid[y][x+1])
+                    cell.right = self.grid[y][x+1]
 
     def show(self):
-        pass
+        for line in self.grid:
+            for cell in line:
+                cell.show()
