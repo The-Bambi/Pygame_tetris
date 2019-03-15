c60fb5cc863c25365f8e3e87adb4dfc8fa3d6fcd
The-bambi <krystian.figiel@smcebi.edu.pl>
2019-03-15 12:53:17 +0100

    Basic controls to blocks

--- grid.py
+++ grid.py
@@ -1,7 +1,9 @@
 from cell import *
+from block import *
+from random import choice
 
 class Grid:
-
+    
     def __init__(self, x_amount, y_amount, screen):
         self.grid = [[Cell(x*29, y*29, 30, 30, screen) for x in range(x_amount)] for y in range(y_amount)]
         for y, line in enumerate(self.grid):
@@ -14,8 +16,27 @@ class Grid:
                     cell.left = self.grid[y][x-1]
                 if x < x_amount - 1:
                     cell.right = self.grid[y][x+1]
+        self.activeBlock = None
 
     def show(self):
         for line in self.grid:
             for cell in line:
                 cell.show()
+
+    def update(self, frameCount):
+        if frameCount%60 == 0:
+            if self.activeBlock is not None and self.activeBlock.active:
+                self.activeBlock.fall()
+
+    def spawn(self):
+        print("spawned")
+        #block_type = choice("IOTSZJL")
+        block_type = "O"
+        if block_type == "O":
+            anchor = self.grid[0][4]
+            self.activeBlock = Block("O", anchor)
+        if block_type == ".":
+            anchor = self.grid[0][4]
+            self.activeBlock = Block(".", anchor)
+
+
