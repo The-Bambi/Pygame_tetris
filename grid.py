0c52d0d6123926e31655385e6cc4fe61a0ac7d50
The-bambi <krystian.figiel@smcebi.edu.pl>
2019-03-16 11:32:32 +0100

    Fixed falling, preparation for all blocks

--- grid.py
+++ grid.py
@@ -2,6 +2,7 @@ from cell import *
 from block import *
 from pile import *
 from random import choice
+from time import time
 
 class Grid:
     def __init__(self, x_amount, y_amount, screen):
@@ -18,6 +19,8 @@ class Grid:
                     cell.right = self.grid[y][x+1]
         self.activeBlock = None
         self.pile = Pile()
+        self.lastFallTime = time()
+        self.fallFreq = 1
 
     def show(self):
         for line in self.grid:
@@ -25,22 +28,28 @@ class Grid:
                 cell.show()
 
     def update(self, frameCount):
-        if frameCount%60 == 0:
+        if time() - self.lastFallTime > self.fallFreq:
             if self.activeBlock is not None:
                 if not self.activeBlock.fall():
                     self.pile.add_block(self.activeBlock)
                     self.spawn()
-            else: self.spawn()
+                self.lastFallTime = time()
 
     def spawn(self):
-        print("spawned")
-        #block_type = choice("IOTSZJL")
-        block_type = "O"
+        block_type = choice("IOTSZJL")
+        if block_type == "I":
+            self.activeBlock = I(self.grid[0][5])
         if block_type == "O":
-            anchor = self.grid[0][4]
-            self.activeBlock = Block("O", anchor)
-        if block_type == ".":
-            anchor = self.grid[0][4]
-            self.activeBlock = Block(".", anchor)
+            self.activeBlock = O(self.grid[0][4])
+        if block_type == "T":
+            self.activeBlock = T(self.grid[0][4])
+        if block_type == "S":
+            self.activeBlock = S(self.grid[0][4])
+        if block_type == "Z":
+            self.activeBlock = Z(self.grid[0][4])
+        if block_type == "J":
+            self.activeBlock = J(self.grid[0][4])
+        if block_type == "L":
+            self.activeBlock = L(self.grid[0][4])
 
 
